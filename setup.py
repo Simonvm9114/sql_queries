2861f4b5a9ea2a6d378562c30c9adc3bf44cf076
Simon van Meegdenburg <49989016+Simonvm9114@users.noreply.github.com>
2020-04-26 12:51:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,17 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sql_query",
-    version="0.0.8",
+    version="0.1.0",
     author="Simon van Meegdenburg",
     author_email="simonvm@live.nl",
-    description="Build simple SQL queries fast",
+    description="Build simple SQL queries fast and clean",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Simonvm9114",
+    url="",
+    projectURLs={
+        "Source code": "https://github.com/Simonvm9114/sql_query",
+        "Youtube": "https://www.youtube.com/playlist?list=PLI4WFrsrAg8sCeBj5xdJ6n79_3Yq3Sz23"
+    }
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
